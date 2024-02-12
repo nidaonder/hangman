dd750448d800b8cf6c2d12d7863918824d5a6718
nidaonder <nidaaonder@gmail.com>
2024-02-13 01:42:26 +0300

    added hangman project

---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const DATA = [
+    { question : 'Bir hayvan adı?', answer : 'eşşek'},
+    { question : 'Bir balık adı?', answer : 'kefal'},
+    { question : 'Bir renk adı?', answer : 'mavi'}
+]
